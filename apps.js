0f19de415bea218a6129171ae25d1ea9d2242f1b
motty-mandel <mandelmottyisrael65@gmail.com>
2026-06-02 10:21:52 +0300

    got the layout started

--- apps.js
+++ apps.js
@@ -29,30 +29,46 @@ function createApp(appName, appWindow) {
         finderCloseResize.classList.add('close-resize');
         appWindow.appendChild(finderCloseResize);
 
-        red = document.createElement('span');
+        // Creating the trafficlight buttons
+        var red = document.createElement('span');
+        red.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#e24b41"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1 17.5 39.1 39.1 39.1z" fill="#ed6a5f"/></g></svg>`
         red.classList.add('red');
-        finderEx = document.createElement('p');
-        finderEx.innerHTML = 'x';
-        finderEx.style.lineHeight = '0.5';
-        finderEx.style.fontSize = 'smaller';
-        finderEx.style.fontFamily = " monospace";
-        red.appendChild(finderEx);
         finderCloseResize.appendChild(red);
+        red.addEventListener('mouseenter', () => {
+            red.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#e24b41"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1 17.5 39.1 39.1 39.1z" fill="#ed6a5f"/><g fill="#460804"><path d="m22.5 57.8 35.3-35.3c1.4-1.4 3.6-1.4 5 0l.1.1c1.4 1.4 1.4 3.6 0 5l-35.3 35.3c-1.4 1.4-3.6 1.4-5 0l-.1-.1c-1.3-1.4-1.3-3.6 0-5z"/><path d="m27.6 22.5 35.3 35.3c1.4 1.4 1.4 3.6 0 5l-.1.1c-1.4 1.4-3.6 1.4-5 0l-35.3-35.3c-1.4-1.4-1.4-3.6 0-5l.1-.1c1.4-1.3 3.6-1.3 5 0z"/></g></g></svg>`
+        });
+        red.addEventListener('mouseleave', () => {
+            red.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#e24b41"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1 17.5 39.1 39.1 39.1z" fill="#ed6a5f"/></g></svg>`
+        });
 
-        yellow = document.createElement('span');
+        var yellow = document.createElement('span');
+        yellow.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#e1a73e"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1 17.5 39.1 39.1 39.1z" fill="#f6be50"/></g></svg>`
         yellow.classList.add('yellow');
         finderCloseResize.appendChild(yellow);
+        yellow.addEventListener('mouseenter', () => {
+            yellow.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#e1a73e"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1 17.5 39.1 39.1 39.1z" fill="#f6be50"/><path d="m17.8 39.1h49.9c1.9 0 3.5 1.6 3.5 3.5v.1c0 1.9-1.6 3.5-3.5 3.5h-49.9c-1.9 0-3.5-1.6-3.5-3.5v-.1c0-1.9 1.5-3.5 3.5-3.5z" fill="#90591d"/></g></svg>`
+        });
+        yellow.addEventListener('mouseleave', () => {
+            yellow.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#e1a73e"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1 17.5 39.1 39.1 39.1z" fill="#f6be50"/></g></svg>`
+        });
 
-        green = document.createElement('span');
+        var green = document.createElement('span');
+        green.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#2dac2f"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1 17.5 39.1 39.1 39.1z" fill="#61c555"/></g></svg>`
         green.classList.add('green');
         finderCloseResize.appendChild(green);
+        green.addEventListener('mouseenter', () => {
+            green.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#2dac2f"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1c0 21.5 17.5 39.1 39.1 39.1z" fill="#61c555"/><path d="m31.2 20.8h26.7c3.6 0 6.5 2.9 6.5 6.5v26.7zm23.2 43.7h-26.8c-3.6 0-6.5-2.9-6.5-6.5v-26.8z" fill="#2a6218"/></g></svg>`
+        });
+        green.addEventListener('mouseleave', () => {
+            green.innerHTML = `<svg enable-background="new 0 0 85.4 85.4" viewBox="0 0 85.4 85.4" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill-rule="evenodd"><path d="m42.7 85.4c23.6 0 42.7-19.1 42.7-42.7s-19.1-42.7-42.7-42.7-42.7 19.1-42.7 42.7 19.1 42.7 42.7 42.7z" fill="#2dac2f"/><path d="m42.7 81.8c21.6 0 39.1-17.5 39.1-39.1s-17.5-39.1-39.1-39.1-39.1 17.5-39.1 39.1 17.5 39.1 39.1 39.1z" fill="#61c555"/></g></svg>`
+        });
 
+        // Setting app header name
         appHeader = document.createElement('p');
         appHeader.classList.add('app-header');
         appHeader.style.fontFamily = '-apple-system, BlinkMacSystemFont, sans-serif';
-        appHeader.style.marginRight = '10px';
         appHeader.textContent = `${appName}`;
-        appWindow.appendChild(appHeader);
+        finderCloseResize.appendChild(appHeader);
 
         document.body.appendChild(appWindow);
         document.getElementById(`${appName}`).classList.add('active');
@@ -65,8 +81,8 @@ function createApp(appName, appWindow) {
         }
         localStorage.setItem(appName, 0);
     }
-    appDragging(appWindow);
-    openClose(finderEx, appWindow, appName);
+    appDragging(appWindow, finderCloseResize);
+    openClose(red, appWindow, appName);
 }
 
 function appDragging(appWindow) {
@@ -118,3 +134,6 @@ function openClose(closeButton, appWindow, appName) {
         appWindow.style.cursor = 'default';
     });
 };
+
+// ------------------------------------Trafficlight button hover
+
--- contextMenu.js
+++ contextMenu.js
@@ -1,4 +1,5 @@
 const customContextMenu = document.getElementById('customContextMenu');
+const contextMenuBorder = document.getElementById('contextMenuBorder');
 
 document.body.addEventListener('contextmenu', (event) => {
   event.preventDefault(); // Prevent default browser context menu
@@ -7,17 +8,23 @@ document.body.addEventListener('contextmenu', (event) => {
   customContextMenu.style.top = `${event.clientY}px`;
   customContextMenu.style.left = `${event.clientX}px`;
 
+  // contextMenuBorder.style.top = `${event.clientY}px`;
+  // contextMenuBorder.style.left = `${event.clientX}px`;
+
   // Show the custom menu
   customContextMenu.classList.add('show');
+  // contextMenuBorder.classList.add('show');
+
 });
 
 // Hide the custom menu when clicking anywhere else
 document.addEventListener('click', () => {
   customContextMenu.classList.remove('show');
+  // contextMenuBorder.classList.remove('show');
 });
 
 // Add functionality to menu items (example)
-customContextMenu.querySelectorAll('div').forEach(item => {
+customContextMenu.querySelectorAll('li').forEach(item => {
   item.addEventListener('click', () => {
     alert(`You clicked: ${item.textContent}`);
     customContextMenu.classList.remove('show'); // Hide after selection
